88ccaf9ef3a863dca1136b49bf0ae92a9d7a54b1
Darren Keen <darren.keen@fanduel.com>
2021-04-17 11:49:54 +0100

    added some game modes

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,13 @@
 export const MODE_KEYS = {
   all: 'All',
   br_brsolo: 'Solos',
+  br_brz_brsolo: 'Solos',
   br_brduos: 'Duos',
+  br_brz_brduos: 'Duos',
   br_brtrios: 'Trios',
+  br_brz_brtrios: 'Trios',
   br_brquads: 'Quads',
+  br_brz_brquads: 'Quads',
   br_dmz_plnbld: 'Blood Money',
   br_dmz_plunquad: 'Blood Money',
 };
